f07630a10c3c82a85604842a1d30a660830d85c7
Cyberstunna <nennii@fomosocial.ca>
2023-09-06 18:36:00 -0600

    setting up client

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import express, {  Request, Response } from "express";
-import grpc, { GrpcObject } from "@grpc/grpc-js";
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader"
+import { DumpstrServiceClient } from "./proto/dumpstr_grpc_pb";
+import { MediaProcessRequest } from "./proto/dumpstr";
 
 const app = express();
 const PORT = 3000;
 
+const packageDefinition = protoLoader.loadSync(`${process.cwd()}/src/proto/dumpstr.proto`, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+})
+const grpcObject = grpc.loadPackageDefinition(packageDefinition);
+const myClient = new DumpstrServiceClient('127.0.0.1:50051', grpc.credentials.createInsecure());
+
+const request = new MediaProcessRequest({
+    
+});
 
 app.use("/upload", (req: Request, res: Response) => {
     try {
