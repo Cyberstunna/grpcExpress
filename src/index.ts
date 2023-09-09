6581edc0e1ed035e0914d38c49855195a303236a
Cyberstunna <nennii@fomosocial.ca>
2023-09-09 16:19:29 -0600

    complete

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express, {  Request, Response } from "express";
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader"
-import { DumpstrServiceClient } from "./proto/dumpstr_grpc_pb";
-import { MediaProcessRequest } from "./proto/dumpstr";
+import { MediaProcessRequest, DumpstrServiceClient } from "./proto/dumpstr";
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json({limit: "5mb"}));
+app.use(express.urlencoded({ extended: true }));
 
 const packageDefinition = protoLoader.loadSync(`${process.cwd()}/src/proto/dumpstr.proto`, {
     keepCase: true,
@@ -17,15 +19,28 @@ const packageDefinition = protoLoader.loadSync(`${process.cwd()}/src/proto/dumps
 const grpcObject = grpc.loadPackageDefinition(packageDefinition);
 const myClient = new DumpstrServiceClient('127.0.0.1:50051', grpc.credentials.createInsecure());
 
-const request = new MediaProcessRequest({
-    
-});
-
 app.use("/upload", (req: Request, res: Response) => {
     try {
-        const {image} = req.body;
-    } catch (error) {
-        
+        const {image, type} = req.body;
+
+        const request = new MediaProcessRequest({
+            type: type,
+            media: image,
+            api_version: '1'
+        });
+
+        myClient.ProcessMediaDump(request, (err, response)=>{
+            if (!err) {
+                console.log('Response:', response?.media_url);
+                res.json({response})
+              } else {
+                console.error('Error:', err.message);
+                res.status(400).send()
+              }
+        })
+    } catch (error: any) {
+        console.error(`Error: ${error.message}`)
+        res.status(400).send()
     }
 })
 
